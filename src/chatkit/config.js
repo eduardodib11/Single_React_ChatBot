846fd33281fd8673b4356aa96dacaad846773c37
Eduardo Dib <eduardodib11@hotmail.com>
2020-07-21 18:01:40 -0300

    Lines Adjustment

--- src/chatkit/config.js
+++ src/chatkit/config.js
@@ -29,14 +29,12 @@ const config = {
             options: [
               {
                 text: "Introduction to JS",
-                url:
-                  "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/",
+                url: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/",
                 id: 1,
               },
               {
                 text: "Mozilla JS Guide",
-                url:
-                  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide",
                 id: 2,
               },
               {
